1177f523b00b672013c20315308b96556d86e613
Nathan Van Ymeren <n@0x85.org>
2026-01-01 19:27:32 -0800

    disable solid cache in prod, use default in-memory store instead

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,11 +44,11 @@ Rails.application.configure do
   config.active_support.report_deprecations = false
 
   # Replace the default in-process memory cache store with a durable alternative.
-  config.cache_store = :solid_cache_store
+  config.cache_store = :memory_store
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :async
-  config.solid_queue.connects_to = { database: { writing: :queue } }
+  #  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
