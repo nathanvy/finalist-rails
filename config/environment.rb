b1eaa2f518e680d946520f38c0362f2e2d7f41fd
Nathan Van Ymeren <n@0x85.org>
2026-01-01 19:38:03 -0800

    load app env vars before app init

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require_relative "application"
 
+# load envvars here so they are preesnt when the app inits
+app_environment_variables = File.join(Rails.root, "config", "app_environment_variables.rb")
+load(app_environment_variables) if File.exists?(app_environment_variables)
+
+
 # Initialize the Rails application.
 Rails.application.initialize!
