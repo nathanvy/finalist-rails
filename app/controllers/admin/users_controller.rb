f1003ee32813e67fb8657f51c487c87f10cc8a00
Nathan Van Ymeren <n@0x85.org>
2026-07-07 23:28:37 -0700

    Prepare v2.5.0

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,43 +1,54 @@
 class Admin::UsersController < Admin::BaseController
+  before_action :set_user, only: %i[show disable enable make_admin remove_admin set_password]
+
   def index
     @users = User.order(:username)
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def disable
-    user = User.find(params[:id])
-    user.update!(disabled_at: Time.current)
+    @user.update!(disabled_at: Time.current)
     redirect_to admin_users_path
   end
 
   def enable
-    user = User.find(params[:id])
-    user.update!(disabled_at: nil)
+    @user.update!(disabled_at: nil)
     redirect_to admin_users_path
   end
 
   def make_admin
-    user = User.find(params[:id])
-    user.update!(admin: true)
+    @user.update!(admin: true)
     redirect_to admin_users_path
   end
 
   def remove_admin
-    user = User.find(params[:id])
-    user.update!(admin: false)
+    @user.update!(admin: false)
     redirect_to admin_users_path
   end
 
   def set_password
-    user = User.find(params[:id])
-    new_password = params[:new_password].to_s
+    if password_params[:password].blank?
+      flash.now[:alert] = "New password can't be blank."
+      return render :show, status: :unprocessable_entity
+    end
 
-    user.update!(password: new_password, password_confirmation: new_password)
-    redirect_to admin_user_path(user), notice: "Password updated."
-  rescue ActiveRecord::RecordInvalid => e
-    redirect_to admin_user_path(user), alert: e.record.errors.full_messages.first
+    if @user.update(password_params)
+      redirect_to admin_user_path(@user), notice: "Password updated."
+    else
+      flash.now[:alert] = @user.errors.full_messages.first || "Password could not be updated."
+      render :show, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
